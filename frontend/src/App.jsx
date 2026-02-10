8e687473ad05fff400aca764623c955d28e35590
linn-dev <tenkotenko.me@gmail.com>
2026-02-10 15:17:31 +0630

    add: cart provider and route cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from './context/cartContext';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/auth/LoginPage';
@@ -13,60 +14,73 @@ import Navbar from './components/common/Navbar';
 import ProtectedRoute from './components/common/ProtectedRoute';
 import AdminRoute from './components/common/AdminRoute';
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import CartPage from './pages/cart/CartPage';
 
 export default function App() {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <div className="min-h-screen bg-linear-to-br from-slate-900 via-blue-900 to-slate-900">
-                    <Navbar />
+                <CartProvider>
+                    <div className="min-h-screen bg-linear-to-br from-slate-900 via-blue-900 to-slate-900">
+                        <Navbar />
 
-                    <Routes>
-                        {/* Public routes */}
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route path="/products" element={<ProductsPage />} />
-                        <Route path="/products/:slug" element={<ProductDetailPage />} />
+                        <Routes>
+                            {/* Public routes */}
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/register" element={<RegisterPage />} />
+                            <Route path="/products" element={<ProductsPage />} />
+                            <Route path="/products/:slug" element={<ProductDetailPage />} />
 
-                        <Route
-                            path="/profile"
-                            element={
-                                <ProtectedRoute>
-                                    <ProfilePage />
-                                </ProtectedRoute>
-                            }
-                        />
+                            <Route
+                                path="/profile"
+                                element={
+                                    <ProtectedRoute>
+                                        <ProfilePage />
+                                    </ProtectedRoute>
+                                }
+                            />
 
-                        <Route
-                            path="/admin/dashboard"
-                            element={
-                                <AdminRoute>
-                                    <AdminDashboard />
-                                </AdminRoute>
-                            }
-                        />
+                            <Route
+                                path="/admin/dashboard"
+                                element={
+                                    <AdminRoute>
+                                        <AdminDashboard />
+                                    </AdminRoute>
+                                }
+                            />
 
-                        <Route
-                            path="/admin/products/create"
-                            element={
-                                <AdminRoute>
-                                    <ProductFormPage />
-                                </AdminRoute>
-                            }
-                        />
+                            <Route
+                                path="/admin/products/create"
+                                element={
+                                    <AdminRoute>
+                                        <ProductFormPage />
+                                    </AdminRoute>
+                                }
+                            />
 
-                        <Route
-                            path="/admin/products/edit/:slug"
-                            element={
-                                <AdminRoute>
-                                    <ProductFormPage />
-                                </AdminRoute>
-                            }
-                        />
-                        <Route path="*" element={<div>Page Not Found</div>} />
-                    </Routes>
-                </div>
+                            <Route
+                                path="/admin/products/edit/:slug"
+                                element={
+                                    <AdminRoute>
+                                        <ProductFormPage />
+                                    </AdminRoute>
+                                }
+                            />
+
+                            <Route
+                                path="/cart"
+                                element={
+                                    <ProtectedRoute>
+                                        <CartPage />
+                                    </ProtectedRoute>
+                                }
+                            />
+
+                            <Route path="*" element={<div>Page Not Found</div>} />
+                        </Routes>
+                    </div>
+                </CartProvider>
             </AuthProvider>
         </BrowserRouter>
     )
