298ca7a5fa4e2680e1089cf3b29a6a8d0a21010c
linn-dev <tenkotenko.me@gmail.com>
2026-02-24 13:14:59 +0630

    refactor(ui): make navbar sticky and update website title

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     const { cartCount } = useCart();
 
     return (
-        <GlassCard className="p-0! rounded-none">
+        <GlassCard className="p-0! rounded-none sticky top-0 z-50">
             <nav className="shadow-md">
                 <div className="container mx-auto px-4">
                     <div className="flex justify-between items-center h-16">
