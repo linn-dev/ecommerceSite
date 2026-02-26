004220a98e3abf6867734ab558a3a555a5d45b71
linn-dev <tenkotenko.me@gmail.com>
2026-02-26 16:57:49 +0630

    add(nav): navigate to get all coupons

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -26,6 +26,9 @@ export default function Navbar() {
                             <Link to="/products" className="hover:text-blue-600">
                                 Products
                             </Link>
+                            <Link to="/coupons" className="hover:text-blue-600">
+                                Coupons
+                            </Link>
                             <Link to="/" className="hover:text-blue-600">
                                 Help & Support
                             </Link>
